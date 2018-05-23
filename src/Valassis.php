c485691ecf7302a1098dec0d249baa33c2d0ebf2
Fred Carlsen <fred@sjelfull.no>
2018-05-23 13:24:29 +0200

    Setup events

--- src/Valassis.php
+++ src/Valassis.php
@@ -10,8 +10,10 @@
 
 namespace superbig\valassis;
 
+use craft\helpers\UrlHelper;
 use superbig\valassis\services\Coupons as CouponsService;
 use superbig\valassis\services\Customers as CustomersService;
+use superbig\valassis\services\Imports as ImportsService;
 use superbig\valassis\variables\ValassisVariable;
 use superbig\valassis\models\Settings;
 
@@ -32,8 +34,9 @@ use yii\base\Event;
  * @package   Valassis
  * @since     1.0.0
  *
- * @property  CouponsService $coupons
+ * @property  CouponsService   $coupons
  * @property  CustomersService $customers
+ * @property  ImportsService   $imports
  */
 class Valassis extends Plugin
 {
@@ -64,44 +67,11 @@ class Valassis extends Plugin
         parent::init();
         self::$plugin = $this;
 
-        Event::on(
-            UrlManager::class,
-            UrlManager::EVENT_REGISTER_SITE_URL_RULES,
-            function (RegisterUrlRulesEvent $event) {
-                $event->rules['siteActionTrigger1'] = 'valassis/coupon';
-                $event->rules['siteActionTrigger2'] = 'valassis/import';
-                $event->rules['siteActionTrigger3'] = 'valassis/customer';
-            }
-        );
+        $this->setComponents([
+            'imports' => ImportsService::class,
+        ]);
 
-        Event::on(
-            UrlManager::class,
-            UrlManager::EVENT_REGISTER_CP_URL_RULES,
-            function (RegisterUrlRulesEvent $event) {
-                $event->rules['cpActionTrigger1'] = 'valassis/coupon/do-something';
-                $event->rules['cpActionTrigger2'] = 'valassis/import/do-something';
-                $event->rules['cpActionTrigger3'] = 'valassis/customer/do-something';
-            }
-        );
-
-        Event::on(
-            CraftVariable::class,
-            CraftVariable::EVENT_INIT,
-            function (Event $event) {
-                /** @var CraftVariable $variable */
-                $variable = $event->sender;
-                $variable->set('valassis', ValassisVariable::class);
-            }
-        );
-
-        Event::on(
-            Plugins::class,
-            Plugins::EVENT_AFTER_INSTALL_PLUGIN,
-            function (PluginEvent $event) {
-                if ($event->plugin === $this) {
-                }
-            }
-        );
+        $this->registerEventListeners();
 
         Craft::info(
             Craft::t(
@@ -111,6 +81,115 @@ class Valassis extends Plugin
             ),
             __METHOD__
         );
+    }
+
+    public function registerEventListeners()
+    {
+
+        Event::on(
+            CraftVariable::class,
+            CraftVariable::EVENT_INIT,
+            function(Event $event) {
+                /** @var CraftVariable $variable */
+                $variable = $event->sender;
+                $variable->set('valassis', ValassisVariable::class);
+            }
+        );
+
+        Event::on(
+            Plugins::class,
+            Plugins::EVENT_AFTER_INSTALL_PLUGIN,
+            function(PluginEvent $event) {
+                if ($event->plugin === $this) {
+                }
+            }
+        );
+
+        Event::on(
+            Plugins::class,
+            Plugins::EVENT_AFTER_LOAD_PLUGINS,
+            function() {
+                // Add in our event listeners that are needed for every request
+                $this->installGlobalEventListeners();
+                // Only respond to non-console site requests
+                $request = Craft::$app->getRequest();
+                if ($request->getIsSiteRequest() && !$request->getIsConsoleRequest()) {
+                    $this->handleSiteRequest();
+                }
+                // AdminCP magic
+                if ($request->getIsCpRequest() && !$request->getIsConsoleRequest()) {
+                    $this->handleCpRequest();
+                }
+            }
+        );
+    }
+
+    public function installGlobalEventListeners()
+    {
+
+    }
+
+    public function handleSiteRequest()
+    {
+        Event::on(Mailer::class, Mailer::EVENT_BEFORE_SEND, function(SendEvent $e) {
+
+        });
+    }
+
+    public function handleCpRequest()
+    {
+        // Plugin events
+        Event::on(
+            Plugins::class,
+            Plugins::EVENT_AFTER_INSTALL_PLUGIN,
+            function(PluginEvent $event) {
+                if ($event->plugin === $this) {
+                    return Craft::$app->getResponse()->redirect(UrlHelper::cpUrl('valassis/settings'));
+                }
+            }
+        );
+
+        Event::on(
+            UrlManager::class,
+            UrlManager::EVENT_REGISTER_CP_URL_RULES,
+            function(RegisterUrlRulesEvent $event) {
+                Craft::debug(
+                    'UrlManager::EVENT_REGISTER_CP_URL_RULES',
+                    __METHOD__
+                );
+                // Register our AdminCP routes
+                $event->rules = array_merge(
+                    $event->rules,
+                    $this->customAdminCpRoutes()
+                );
+            }
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getCpNavItem(): array
+    {
+        $nav           = parent::getCpNavItem();
+        $nav['subnav'] = [
+            'coupons' => ['label' => 'Coupons', 'url' => UrlHelper::cpUrl('valassis/coupons')],
+            'imports' => ['label' => 'Imports', 'url' => UrlHelper::cpUrl('valassis/imports')],
+        ];
+
+        return $nav;
+    }
+
+    protected function customAdminCpRoutes(): array
+    {
+        return [
+            'valassis/coupons'          => 'valassis/coupon/index',
+            'valassis/coupons/<id:\d+>' => 'valassis/coupon/edit',
+            'valassis/imports'          => 'valassis/import/index',
+            'valassis/imports/new'      => 'valassis/import/new',
+            'valassis/imports/<id:\d+>' => 'valassis/import/edit',
+            //'valassis/settings'         => 'valassis/settings',
+        ];
     }
 
     // Protected Methods
@@ -132,7 +211,7 @@ class Valassis extends Plugin
         return Craft::$app->view->renderTemplate(
             'valassis/settings',
             [
-                'settings' => $this->getSettings()
+                'settings' => $this->getSettings(),
             ]
         );
     }
